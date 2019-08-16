7e459b993c5b902659896a6c83282b1c70d144b8
VictorGil <victorgilsanz@gmail.com>
2019-08-16 12:43:21 +0200

    Added TransferProcessor interface and a simple implementing class

---
+++ src/main/java/net/devaction/kafka/transfersrecordingservice/transferconsumer/SimpleTransferProcessorImpl.java
@@ -0,0 +1,20 @@
+package net.devaction.kafka.transfersrecordingservice.transferconsumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.devaction.kafka.avro.Transfer;
+
+/**
+ * @author Víctor Gil
+ *
+ * since August 2019
+ */
+public class SimpleTransferProcessorImpl implements TransferProcessor{
+    private static final Logger log = LoggerFactory.getLogger(SimpleTransferProcessorImpl.class);
+
+    @Override
+    public void process(Transfer transfer){
+        log.info("Transfer data processed: {}", transfer);        
+    }
+}
---
+++ src/main/java/net/devaction/kafka/transfersrecordingservice/transferconsumer/TransferProcessor.java
@@ -0,0 +1,13 @@
+package net.devaction.kafka.transfersrecordingservice.transferconsumer;
+
+import net.devaction.kafka.avro.Transfer;
+
+/**
+ * @author Víctor Gil
+ *
+ * since August 2019
+ */
+public interface TransferProcessor{
+    
+    public void process(Transfer transfer);
+}
