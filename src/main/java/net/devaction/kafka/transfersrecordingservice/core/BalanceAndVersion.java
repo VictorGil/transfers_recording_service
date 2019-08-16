b5c86a42ad3583197c1c77bd9b0aec6a5bd9c5a7
VictorGil <victorgilsanz@gmail.com>
2019-08-16 19:45:51 +0200

    Added equals and hashcode

--- src/main/java/net/devaction/kafka/transfersrecordingservice/core/BalanceAndVersion.java
+++ src/main/java/net/devaction/kafka/transfersrecordingservice/core/BalanceAndVersion.java
@@ -22,6 +22,42 @@ public class BalanceAndVersion{
         return "BalanceAndVersion [balance=" + balance + ", version=" + version + "]";
     }
 
+    @Override
+    public int hashCode(){
+        final int prime = 31;
+        int result = 1;
+        
+        result = prime * result + ((balance == null) ? 0 : balance.hashCode());
+        result = prime * result + (int) (version ^ (version >>> 32));
+        
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj)
+            return true;
+        
+        if (obj == null)
+            return false;
+        
+        if (getClass() != obj.getClass())
+            return false;
+        
+        BalanceAndVersion other = (BalanceAndVersion) obj;
+        
+        if (balance == null){
+            if (other.balance != null)
+                return false;
+            // Note that we should not use "equals" method
+            // to find out whether two BigDecimals represent
+            // the same numeric value
+        } else if (balance.compareTo(other.balance) != 0)
+            return false;
+        
+        return version == other.version;
+    }
+
     public BigDecimal getBalance(){
         return balance;
     }
