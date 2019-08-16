9868ae2909e89a1569825623e7366454c95e209a
VictorGil <victorgilsanz@gmail.com>
2019-08-16 23:53:00 +0200

    Adding the required code to AccountBalanceProducerTester which was empty

--- src/test/java/net/devaction/kafka/transfersrecordingservice/accountbalanceproducer/AccountBalanceProducerTester.java
+++ src/test/java/net/devaction/kafka/transfersrecordingservice/accountbalanceproducer/AccountBalanceProducerTester.java
@@ -1,10 +1,52 @@
 package net.devaction.kafka.transfersrecordingservice.accountbalanceproducer;
 
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.devaction.entity.AccountBalanceEntity;
+import net.devaction.kafka.transfersrecordingservice.config.ConfigReader;
+import net.devaction.kafka.transfersrecordingservice.config.ConfigValues;
+
 /**
  * @author Víctor Gil
  *
  * since August 2019
  */
 public class AccountBalanceProducerTester{
+    private static final Logger log = LoggerFactory.getLogger(
+            AccountBalanceProducerTester.class);
     
+    public static void main(String[] args){
+        new AccountBalanceProducerTester().run();
+    }
+    
+    private void run() {
+        AccountBalanceProducer producer = new AccountBalanceProducerImpl();
+        
+        ConfigValues configValues;
+        try{
+            configValues = new ConfigReader().read();
+        } catch (Exception ex){
+            log.error("Unable to read the configuration values, exiting");
+            return;
+        }
+        
+        producer.start(configValues.getBootstrapServers(), 
+                configValues.getSchemaRegistryUrl());
+        
+        AccountBalanceEntity abEntity = new AccountBalanceEntity(
+                "28a090daa001", "334490daa001");
+        
+        producer.send(abEntity);
+        log.info("Sleeping while the message is sent");
+        try{
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException ex){
+            log.error(ex.toString(), ex);
+        }
+        
+        producer.stop();        
+    }
 }
